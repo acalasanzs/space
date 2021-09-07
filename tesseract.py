740bdf4d58beb7d8bdb0ae469f192da60aee61f5
Albert Calasanz <82325698+acsstudios@users.noreply.github.com>
2021-09-07 18:56:20 +0200

    Update tesseract.py

--- tesseract.py
+++ tesseract.py
@@ -2,24 +2,25 @@ import pygame
 from win32api import GetSystemMetrics
 import os
 import math
-def matrix_m(a,b):
+def matrix_multiplication(a, b):
     columns_a = len(a[0])
     rows_a = len(a)
-    columns_b = len (b[0])
+    columns_b = len(b[0])
     rows_b = len(b)
 
     result_matrix = [[j for j in range(columns_b)] for i in range(rows_a)]
-    if columns_a == rows_a:
+    if columns_a == rows_b:
         for x in range(rows_a):
             for y in range(columns_b):
-                sum= 0
-                for j in range(columns_a):
-                    sum += range(columns_a)
+                sum = 0
+                for k in range(columns_a):
+                    sum += a[x][k] * b[k][y]
                 result_matrix[x][y] = sum
 
         return result_matrix
+
     else:
-        print("Err")
+        print("columns of the first matrix must be equal to the rows of the second matrix")
         return None
 
 os.environ["SDL_VIDEO_CENTERED"]= '1'
@@ -69,4 +70,41 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     index = 0
-    projected_point = [j]
+    projected_point = [j for j in range(len(points))]
+
+    #3d rotation matrices
+
+    rotation_x = [[1,0,0],
+                  [0,math.cos(angle),-math.sin(angle)],
+                  [0,math.sin(angle),math.cos(angle)]]
+    #4d matrix rotations
+    rotation_xy = [[math.cos(angle), -math.sin(angle), 0, 0],
+                  [math.sin(angle), math.cos(angle), 0, 0],
+                  [0,0,1,0],
+                  [0,0,0,1]
+                  ]
+
+    rotation_zw = [[1,0,0,0],
+                  [0,1,0,0],
+                  [0,0,math.cos(angle),-math.sin(angle)],
+                  [0,0,math.sin(angle),math.cos(angle)]
+                  ]
+
+    for point in points:
+
+        rotated_3d = matrix_m(rotation_xy, point)
+        rotated_3d = matrix_m(rotation_zw, rotated_3d)
+
+        distance = 5
+
+        w = 1/ (distance - rotated_3d[3][0])
+        projection_matrix4d = [[w,0,0,0],
+                               [0,w,0,0],
+                               [0,0,w,0]
+                               ]
+        projected_3d = matrix_m(projection_matrix4d,rotated_3d)
+        rotated2d = matrix_m(rotation_x, projected_3d)
+        z = 1/(distance - (rotated2d[2][0] + ror))
+    pygame.display.update()
+
+pygame.quit()
